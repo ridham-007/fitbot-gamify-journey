e4ea756d6bfa909d1c4bd49919537d979d6c5506
Ridham Avaiya <ridhamavaiya1999@gmail.com>
2025-04-20 09:58:54 +0530

    Fixed based url issue

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -123,18 +123,6 @@ const Signup = () => {
             {currentStep === 1 ? (
               <>
                 <AccountForm onSubmit={handleAccountSubmit} />
-                <div className="mt-6">
-                  <div className="relative">
-                    <div className="absolute inset-0 flex items-center">
-                      <div className="w-full border-t border-gray-300 dark:border-fitDark-600" />
-                    </div>
-                    <div className="relative flex justify-center text-sm">
-                      <span className="px-2 bg-white dark:bg-fitDark-800 text-gray-500 dark:text-gray-400">
-                        Or continue with
-                      </span>
-                    </div>
-                  </div>
-                </div>
               </>
             ) : (
               <ProfileForm 
